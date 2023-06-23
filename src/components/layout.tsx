b9ab5c9d0d2dad0354398094df16cf6bdafb0b0b
Sandeep Kumar <satyasandeep786@gmail.com>
2023-06-23 12:52:08 +0530

    refactor: Remove old context

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -170,7 +170,8 @@ export function Hero(props) {
                             <h1 className="text-[24px]  leading-[28px] md:text-[64px] md:leading-[72px] font-[500] mb-[24px]">{title}</h1>
                             <p className="w-full md:w-[70%] text-[16px] text-[#67666E] leading-[24px] font-[500] mb-[24px]">{subtitle}</p>
                             <Button><a target={"_blank"} href={"https://app.meroku.org"}>{button.text}</a></Button>
-                        </div>
+							<Button className="ml-10" onClick={openTransak}>Buy Crypto</Button>
+						</div>
                     </Row>
                 </div>
                 <div
@@ -397,14 +398,10 @@ export default function Layout(props) {
                                     : buildLoadingCard(5)}
                             </Slider>
                         </div>
-						<div className="container relative">
-								<Row className="justify-between items-center my-[32px]">
-									<Button onClick={openTransak}>Stake USDC</Button>
-								</Row>
-							</div>
                     </section>
                     }
                     {props.children}
+					<ScrollToTopButton />
                 </main>
             </div>
         </>
