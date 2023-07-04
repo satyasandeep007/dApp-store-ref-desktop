ecb3290d5e2d630bdee8e2f759800678bb412390
Sandeep Kumar <satyasandeep786@gmail.com>
2023-07-04 21:09:29 +0530

    feat: Add whitelabel params to transak integration

--- src/utils/transak.ts
+++ src/utils/transak.ts
@@ -1,43 +1,27 @@
 import transakSDK from "@transak/transak-sdk";
 
 const initializeTransak = (params) => {
-  const {
-    apiKey,
-    environment,
-    defaultCryptoCurrency,
-    walletAddress,
-    fiatCurrency,
-    partnerOrderId,
-    email,
-  } = params;
-
   console.log("launching transak");
-  let transak = new transakSDK({
-    widgetHeight: "750px",
-    widgetWidth: "450px",
-    fiatCurrency, // If you want to limit fiat selection eg 'GBP'
-    apiKey, // Your API Key
-    environment, // STAGING/PRODUCTION
-    defaultCryptoCurrency,
-    email,
-    walletAddress,
-    partnerOrderId,
-    disableWalletAddressForm: true,
-    // Need to add transakone params and other needed params
-  });
-
+  let transak = new transakSDK(params);
   return transak;
 };
 
 export const openTransak = ({ email, address }) => {
   const transakParams = {
-    apiKey: "3758451a-36b9-4f92-a8a5-192bdf9d76de",
+    apiKey: "a2374be4-c59a-400e-809b-72c226c74b8f",
     environment: "STAGING",
-    defaultCryptoCurrency: "APOLDAI",
+    cryptoCurrencyCode: "USDT",
     walletAddress: address,
     fiatCurrency: "EUR",
+    fiatAmount: 300,
     network: "polygon",
+    paymentMethod: "credit_debit_card",
     email: email || "sandeep@transak.com",
+    hideExchangeScreen: true,
+    disableWalletAddressForm: true,
+    isBuyOrSell: "BUY",
+    widgetHeight: "750px",
+    widgetWidth: "450px",
   };
   console.log(transakParams, "transakParams");
 
